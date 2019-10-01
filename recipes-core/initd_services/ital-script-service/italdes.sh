8a660acb45a54460114169182c3bda6198048bc3
marco <marco.sinigaglia@italdes.it>
2019-10-01 14:32:03 +0200

    fix for italdes script for ip  static configuration

--- recipes-core/initd_services/ital-script-service/italdes.sh
+++ recipes-core/initd_services/ital-script-service/italdes.sh
@@ -76,7 +76,7 @@ dmesg | grep "time is not reliable" && touch /var/run/rtc_invalid_date
 
 #altrimenti imposto indirizzo IP statico
 #i parametri sono già presenti nel file /etc/network/interfaces
-else
+if [ -e "/home/root/nameserver.txt" ]; then
   echo "ETH: Configurazione statica"
   ifdown eth0
   ifup eth0
