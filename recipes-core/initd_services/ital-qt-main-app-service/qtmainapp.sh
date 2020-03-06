64e6833d97d081ee344f22848c7984cad8cef8fa
marco <marco.sinigaglia@italdes.it>
2020-03-06 17:23:01 +0100

    qt writes internally in a file (does not use redirect anymore)
    JP-603

--- recipes-core/initd_services/ital-qt-main-app-service/qtmainapp.sh
+++ recipes-core/initd_services/ital-qt-main-app-service/qtmainapp.sh
@@ -5,20 +5,14 @@ SCRIPT=$2
 LOGFILE=$3
 
 logger -s "LOOPFILE: $LOOPFILE"
-logger -s "LOGFILE: $LOGFILE"
 logger -s "SCRIPT: $SCRIPT"
 
 #  $SCRIPT &> sed '/^WARNING: cipher_setiv/d' > $LOGFILE
 
-cmd() {
-  $SCRIPT &>>  $LOGFILE
-}
-
 LOOP=$(cat "$LOOPFILE")
 while [ $LOOP == "true" ]; do
   logger -s "LOOP: $LOOP"
-  logger -s "CMD: $CMD"
-  cmd
+  $SCRIPT
   sleep 3
   LOOP=$(cat "$LOOPFILE")
 done
