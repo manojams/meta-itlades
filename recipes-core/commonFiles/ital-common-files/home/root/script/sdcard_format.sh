a973f69f73acf18b0151e48e5f6839150f1aab97
Antonio Manco <antonio.manco@italdes.it>
2020-04-29 15:36:39 +0200

    updated sdcard format script with umount command

--- recipes-core/commonFiles/ital-common-files/home/root/script/sdcard_format.sh
+++ recipes-core/commonFiles/ital-common-files/home/root/script/sdcard_format.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
+umount -fl /dev/mmcblk0p1
+if [ $? -ne 0 ]; then
+        echo "Warning when running umount /dev/mmcblk0p1"
+	#no exit is necessary
+        #exit 1
+fi
+
 fdisk /dev/mmcblk0 <<'EOF'
 d
+p
 n
 p
 1
 
+w
+EOF
+
+if [ $? -ne 0 ]; then
+        echo "Error fdisk new partition"
+        exit 1
+fi
+
+fdisk /dev/mmcblk0 <<'EOF'
 t
 83
 w
 EOF
 
 if [ $? -ne 0 ]; then
-        echo "Error fdisk"
+        echo "Error fdisk partition type"
         exit 1
 fi
 
-mkfs.ext4 /dev/mmcblk0 <<'EOF'
+mkfs.ext4 /dev/mmcblk0p1 <<'EOF'
 y
 EOF
 
@@ -25,3 +42,4 @@ if [ $? -ne 0 ]; then
 fi
 
 exit $?
+
